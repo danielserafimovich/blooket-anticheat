405937fa127aaecd63b03c135a5e1962e98067b7
danielserafimovich <119928204+danielserafimovich@users.noreply.github.com>
2023-03-03 14:00:23 +0000

    Create check.js

---
+++ check.js
@@ -0,0 +1,2 @@
+// This code checks the users stats each time: Upon joining this proggram saves your beggining score, everytime you advance it will keep measuring.
+// If a student advnaces more than 3x their original score, they will be given a screen that warns then and shows them a choiceboad for punishments upon their next options.
