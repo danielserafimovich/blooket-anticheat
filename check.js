ca5fd6f242b0b1157efccca5cffe42aa9fc60ece
danielserafimovich <119928204+danielserafimovich@users.noreply.github.com>
2023-03-03 14:04:47 +0000

    Update check.js

--- check.js
+++ check.js
@@ -1,2 +1,5 @@
 // This code checks the users stats each time: Upon joining this proggram saves your beggining score, everytime you advance it will keep measuring.
-// If a student advnaces more than 3x their original score, they will be given a screen that warns then and shows them a choiceboad for punishments upon their next options.
+/// If a student advnaces more than 3x their original score, they will be given a screen that warns then and shows them a choiceboad for punishments upon their next option set.
+//// Your students device will not work with blooket for the next few months, you can use teachers authority to unlock it.
+
+// Crypto hack
